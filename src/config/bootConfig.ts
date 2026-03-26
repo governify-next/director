b57ed53d8bc0f528afd1542d6a0e90912a165967
León Martín <leonmartinl@icloud.com>
2026-03-26 17:37:01 +0100

    feat: add env variables

--- src/config/bootConfig.ts
+++ src/config/bootConfig.ts
@@ -7,7 +7,8 @@ dotenv.config({ path: envPath });
 
 export const bootEnv = {
     GOV_LOG_LEVEL: process.env.GOV_LOG_LEVEL || 'INFO',
-    GOV_SERVICE_NAME: process.env.GOV_SERVICE_NAME || 'UnknownService',
+    GOV_SERVICE_NAME: process.env.GOV_SERVICE_NAME || 'Director',
     PORT: process.env.PORT || '3000',
     MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/mern_example',
+    REDIS_URI: process.env.REDIS_URI || 'redis://localhost:6379',
 };
