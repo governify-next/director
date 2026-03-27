6f5706c8ce0b5956a8174ca1dda767afb94a1273
León Martín <leonmartinl@icloud.com>
2026-03-27 22:01:58 +0100

    feat: comment on script registry, for readability

--- src/scripts/registry.ts
+++ src/scripts/registry.ts
@@ -1,6 +1,7 @@
 import { ScriptHandler } from '../types/script.js';
 import exampleEcho from './example.echo.js';
 
+// moduleRef -> ScriptHandler
 const scriptRegistry: Record<string, ScriptHandler> = {
     'example.echo': exampleEcho,
 };
