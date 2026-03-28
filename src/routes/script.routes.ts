62da536f701d0666f679fb24b63635d3a284b6e8
León Martín <leonmartinl@icloud.com>
2026-03-28 19:06:32 +0100

    fix: script routes

--- src/routes/script.routes.ts
+++ src/routes/script.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import * as scriptController from '../controllers/script.controller.js';
-import { validateCreateScript, validateUpdateScript } from '../middlewares/script.validator.js';
+import { validateScript } from '../middlewares/script.validator.js';
 import { validateMongoId } from '../middlewares/mongoId.validator.js';
 
 export const scriptRoutes = Router();
 
 scriptRoutes.get('/', scriptController.getScripts);
 scriptRoutes.get('/:id', validateMongoId, scriptController.getScriptById);
-scriptRoutes.post('/', validateCreateScript, scriptController.createScript);
-scriptRoutes.put('/:id', validateMongoId, validateUpdateScript, scriptController.updateScript);
+scriptRoutes.post('/', validateScript, scriptController.createScript);
+scriptRoutes.put('/:id', validateMongoId, validateScript, scriptController.updateScript);
 scriptRoutes.delete('/:id', validateMongoId, scriptController.deleteScript);
