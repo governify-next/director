27d3d188a4b192641ea313d37302b722c7a90421
León Martín <leonmartinl@icloud.com>
2026-03-28 19:13:43 +0100

    fix: script service id type

--- src/services/script.service.ts
+++ src/services/script.service.ts
@@ -2,6 +2,7 @@ import * as scriptRepository from '../repositories/script.repository.js';
 import { IScript } from '../models/script.model.js';
 import { ScriptHandler } from '../types/script.js';
 import scriptRegistry from '../scripts/registry.js';
+import { Types } from 'mongoose';
 
 export const createScript = async (data: Partial<IScript>) => {
     return await scriptRepository.createScript(data);
@@ -11,15 +12,15 @@ export const getScripts = async () => {
     return await scriptRepository.getScripts();
 };
 
-export const getScriptById = async (id: string) => {
+export const getScriptById = async (id: Types.ObjectId) => {
     return await scriptRepository.getScriptById(id);
 };
 
-export const updateScript = async (id: string, data: Partial<IScript>) => {
+export const updateScript = async (id: Types.ObjectId, data: Partial<IScript>) => {
     return await scriptRepository.updateScript(id, data);
 };
 
-export const deleteScript = async (id: string) => {
+export const deleteScript = async (id: Types.ObjectId) => {
     return await scriptRepository.deleteScript(id);
 };
 
