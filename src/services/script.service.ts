014146e5ec1fada1c8eb9bcf864055e82e32e73d
León Martín <leonmartinl@icloud.com>
2026-03-28 16:41:00 +0100

    feat: move script loader to script service

--- src/services/script.service.ts
+++ src/services/script.service.ts
@@ -1,5 +1,7 @@
 import * as scriptRepository from '../repositories/script.repository.js';
 import { IScript } from '../models/script.model.js';
+import { ScriptHandler } from '../types/script.js';
+import scriptRegistry from '../scripts/registry.js';
 
 export const createScript = async (data: Partial<IScript>) => {
     return await scriptRepository.createScript(data);
@@ -20,3 +22,13 @@ export const updateScript = async (id: string, data: Partial<IScript>) => {
 export const deleteScript = async (id: string) => {
     return await scriptRepository.deleteScript(id);
 };
+
+export async function loadScriptHandler(moduleRef: string): Promise<ScriptHandler> {
+    const scriptHandle = scriptRegistry[moduleRef];
+
+    if (typeof scriptHandle !== 'function') {
+        throw new Error(`Script module must export a default function: ${moduleRef}`);
+    }
+
+    return scriptHandle;
+}
