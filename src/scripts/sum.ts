9a2c58c0c9215f40f2c97a406003b398e9525f89
León Martín <leonmartinl@icloud.com>
2026-03-29 18:34:49 +0200

    feat: make the sum script more verbose for demos

--- src/scripts/sum.ts
+++ src/scripts/sum.ts
@@ -4,7 +4,7 @@ const sum: ScriptHandler = async (args, context) => {
     const a = args.a as number;
     const b = args.b as number;
 
-    return a + b;
+    return `Hello from the sum script! ${a} + ${b} = ${a + b}`;
 };
 
 export default sum;
