0818a035caac751d54643fce1115294e6603d89e
León Martín <leonmartinl@icloud.com>
2026-03-29 19:16:11 +0200

    refactor: style

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,11 +1,11 @@
 import * as taskRepository from '../repositories/task.repository.js';
+import * as taskScheduler from '../workers/taskScheduler.js';
 import { ITask } from '../models/task.model.js';
-import { scheduleTask, removeTask } from '../workers/taskScheduler.js';
 
 export const createTask = async (data: Partial<ITask>) => {
     const task = await taskRepository.createTask(data);
 
-    await scheduleTask(task);
+    await taskScheduler.scheduleTask(task);
     return task;
 };
 
@@ -24,8 +24,8 @@ export const updateTask = async (id: string, data: Partial<ITask>) => {
     const updatedTask = await taskRepository.updateTask(id, data);
     if (!updatedTask) return null;
 
-    await removeTask(currentTask);
-    await scheduleTask(updatedTask);
+    await taskScheduler.removeTask(currentTask);
+    await taskScheduler.scheduleTask(updatedTask);
     return updatedTask;
 };
 
@@ -33,7 +33,7 @@ export const deleteTask = async (id: string) => {
     const deletedTask = await taskRepository.deleteTask(id);
     if (!deletedTask) return null;
 
-    await removeTask(deletedTask);
+    await taskScheduler.removeTask(deletedTask);
     return deletedTask;
 };
 
