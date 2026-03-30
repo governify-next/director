8582156994f53a0a7d660bfbfe86d149f3f721cb
León Martín <leonmartinl@icloud.com>
2026-03-30 10:08:17 +0200

    feat: improve script for debugging

--- src/scripts/sum.ts
+++ src/scripts/sum.ts
@@ -1,9 +1,11 @@
-import { ScriptHandler } from '../types/script.js';
+import { ScriptHandler, TaskExecutionContext } from '../types/script.js';
 
-const sum: ScriptHandler = async (args, context) => {
+const sum: ScriptHandler = async (args, context: TaskExecutionContext) => {
     const a = args.a as number;
     const b = args.b as number;
+    const taskId = context.taskId;
 
+    console.log(`Executing sum script for task ${taskId}.`);
     return `Hello from the sum script! ${a} + ${b} = ${a + b}`;
 };
 
