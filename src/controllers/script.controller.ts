6e2bbbdf77858f7f2c2b4e097ac2d5f8fb6020b7
León Martín <leonmartinl@icloud.com>
2026-03-30 18:06:12 +0200

    feat: remove Script from db (wip)

--- src/controllers/script.controller.ts
+++ src/controllers/script.controller.ts
@@ -3,15 +3,6 @@ import * as scriptService from '../services/script.service.js';
 import { sendSuccess } from '../utils/standardResponse.js';
 import { NotFoundError } from '../utils/customErrors.js';
 
-export const createScript = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const script = await scriptService.createScript(req.body);
-        return sendSuccess(res, { data: script, httpStatus: 201, message: 'Script created' });
-    } catch (err) {
-        next(err);
-    }
-};
-
 export const getScripts = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const scripts = await scriptService.getScripts();
@@ -21,31 +12,11 @@ export const getScripts = async (req: Request, res: Response, next: NextFunction
     }
 };
 
-export const getScriptById = async (req: Request, res: Response, next: NextFunction) => {
+export const getScriptByName = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const script = await scriptService.getScriptById(req.params.id);
+        const script = await scriptService.getScriptByName(req.params.name);
         if (!script) throw new NotFoundError('Script not found');
         return sendSuccess(res, { data: script });
-    } catch (err) {
-        next(err);
-    }
-};
-
-export const updateScript = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const script = await scriptService.updateScript(req.params.id, req.body);
-        if (!script) throw new NotFoundError('Script not found');
-        return sendSuccess(res, { data: script, message: 'Script updated' });
-    } catch (err) {
-        next(err);
-    }
-};
-
-export const deleteScript = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const script = await scriptService.deleteScript(req.params.id);
-        if (!script) throw new NotFoundError('Script not found');
-        return sendSuccess(res, { data: null, message: 'Script deleted' });
     } catch (err) {
         next(err);
     }
--- src/routes/script.routes.ts
+++ src/routes/script.routes.ts
@@ -1,12 +1,7 @@
 import { Router } from 'express';
 import * as scriptController from '../controllers/script.controller.js';
-import { validateScript } from '../middlewares/script.validator.js';
-import { validateMongoId } from '../middlewares/mongoId.validator.js';
 
 export const scriptRoutes = Router();
 
 scriptRoutes.get('/', scriptController.getScripts);
-scriptRoutes.get('/:id', validateMongoId, scriptController.getScriptById);
-scriptRoutes.post('/', validateScript, scriptController.createScript);
-scriptRoutes.put('/:id', validateMongoId, validateScript, scriptController.updateScript);
-scriptRoutes.delete('/:id', validateMongoId, scriptController.deleteScript);
+scriptRoutes.get('/:name', scriptController.getScriptByName);
