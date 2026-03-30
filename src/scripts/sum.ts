28d0c368e348f4e6199c8c31c8a87e0522b10ff7
León Martín <leonmartinl@icloud.com>
2026-03-30 11:47:42 +0200

    refactor: use new logger in scripts

--- src/scripts/sum.ts
+++ src/scripts/sum.ts
@@ -4,8 +4,9 @@ const sum: ScriptHandler = async (args, context: TaskExecutionContext) => {
     const a = args.a as number;
     const b = args.b as number;
     const taskId = context.taskId;
+    const logger = context.logger;
 
-    console.log(`Executing sum script for task ${taskId}.`);
+    logger.info(`Executing sum script for task ${taskId}.`);
     return `Hello from the sum script! ${a} + ${b} = ${a + b}`;
 };
 
