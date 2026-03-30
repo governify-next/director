ce3ed09456af5fb64b402facc42750d3d08139c1
León Martín <leonmartinl@icloud.com>
2026-03-30 18:35:33 +0200

    feat: new script repo and service

--- src/services/script.service.ts
+++ src/services/script.service.ts
@@ -1,34 +1,9 @@
 import * as scriptRepository from '../repositories/script.repository.js';
-import { IScript } from '../models/script.model.js';
-import { ScriptHandler } from '../types/script.js';
-import scriptRegistry from '../scripts/registry.js';
 
-export const createScript = async (data: Partial<IScript>) => {
-    return await scriptRepository.createScript(data);
+export const getScripts = () => {
+    return scriptRepository.getScripts();
 };
 
-export const getScripts = async () => {
-    return await scriptRepository.getScripts();
+export const getScriptByName = (name: string) => {
+    return scriptRepository.getScriptByName(name);
 };
-
-export const getScriptById = async (id: string) => {
-    return await scriptRepository.getScriptById(id);
-};
-
-export const updateScript = async (id: string, data: Partial<IScript>) => {
-    return await scriptRepository.updateScript(id, data);
-};
-
-export const deleteScript = async (id: string) => {
-    return await scriptRepository.deleteScript(id);
-};
-
-export async function loadScriptHandler(moduleRef: string): Promise<ScriptHandler> {
-    const scriptHandle = scriptRegistry[moduleRef];
-
-    if (typeof scriptHandle !== 'function') {
-        throw new Error(`Script module must export a default function: ${moduleRef}`);
-    }
-
-    return scriptHandle;
-}
