818207b6c26efdec753d769453767fb3b1ff7b80
León Martín <leonmartinl@icloud.com>
2026-09-21 17:03:26 +0200

    feat: protect all routes behind svc auth

--- src/routes/script.routes.ts
+++ src/routes/script.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import * as scriptController from '../controllers/script.controller.js';
+import { checkServiceAuthentication } from '../integrations/authenticator.validator.js';
 
 export const scriptRoutes = Router();
 
-scriptRoutes.get('/scripts/', scriptController.getScripts);
-scriptRoutes.get('/scripts/:name', scriptController.getScriptByName);
+scriptRoutes.get('/scripts/', checkServiceAuthentication, scriptController.getScripts);
+scriptRoutes.get('/scripts/:name', checkServiceAuthentication, scriptController.getScriptByName);
--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -7,33 +7,73 @@ import {
     validateTaskUpdateFilters,
 } from '../middlewares/task.validator.js';
 import { validateMongoId } from '../middlewares/mongoId.validator.js';
+import { checkServiceAuthentication } from '../middlewares/authenticator.validator.js';
 
 export const taskRoutes = Router();
 
-taskRoutes.get('/tasks/', taskController.getTasks);
-taskRoutes.post('/tasks/search', validateTaskFilters, taskController.searchTasks);
+taskRoutes.get('/tasks/', checkServiceAuthentication, taskController.getTasks);
+taskRoutes.post(
+    '/tasks/search',
+    checkServiceAuthentication,
+    validateTaskFilters,
+    taskController.searchTasks,
+);
 taskRoutes.post(
     '/tasks/search/delete',
+    checkServiceAuthentication,
     validateTaskDeleteFilters,
     taskController.deleteTasksByFilters,
 );
 taskRoutes.post(
     '/tasks/search/enable',
+    checkServiceAuthentication,
     validateTaskUpdateFilters,
     taskController.enableTasksByFilters,
 );
 taskRoutes.post(
     '/tasks/search/disable',
+    checkServiceAuthentication,
     validateTaskUpdateFilters,
     taskController.disableTasksByFilters,
 );
-taskRoutes.get('/tasks/:id', validateMongoId, taskController.getTaskById);
-taskRoutes.post('/tasks/', validateTask, taskController.createTask);
-taskRoutes.delete('/tasks/', taskController.deleteAllTasks);
-taskRoutes.put('/tasks/:id', validateMongoId, validateTask, taskController.updateTask);
-taskRoutes.delete('/tasks/:id', validateMongoId, taskController.deleteTask);
+taskRoutes.get(
+    '/tasks/:id',
+    checkServiceAuthentication,
+    validateMongoId,
+    taskController.getTaskById,
+);
+taskRoutes.post('/tasks/', checkServiceAuthentication, validateTask, taskController.createTask);
+taskRoutes.delete('/tasks/', checkServiceAuthentication, taskController.deleteAllTasks);
+taskRoutes.put(
+    '/tasks/:id',
+    checkServiceAuthentication,
+    validateMongoId,
+    validateTask,
+    taskController.updateTask,
+);
+taskRoutes.delete(
+    '/tasks/:id',
+    checkServiceAuthentication,
+    validateMongoId,
+    taskController.deleteTask,
+);
 
-taskRoutes.post('/tasks/:id/enable', validateMongoId, taskController.enableTask);
-taskRoutes.post('/tasks/:id/disable', validateMongoId, taskController.disableTask);
+taskRoutes.post(
+    '/tasks/:id/enable',
+    checkServiceAuthentication,
+    validateMongoId,
+    taskController.enableTask,
+);
+taskRoutes.post(
+    '/tasks/:id/disable',
+    checkServiceAuthentication,
+    validateMongoId,
+    taskController.disableTask,
+);
 
-taskRoutes.get('/tasks/:id/executions', validateMongoId, taskController.getTaskExecutions);
+taskRoutes.get(
+    '/tasks/:id/executions',
+    checkServiceAuthentication,
+    validateMongoId,
+    taskController.getTaskExecutions,
+);
